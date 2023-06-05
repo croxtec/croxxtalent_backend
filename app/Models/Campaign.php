e932d918acdb7b9d69299bde08b837d865b0fc52
vearse <versatilekaywize94@gmail.com>
2023-06-05 16:16:02 +0100

    Campaign core

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -184,10 +184,10 @@ class Campaign extends Model
     //     return $this->belongsToMany('App\Models\SkillTertiary', 'campaign_skill', 'campaign_id', 'skill_id');
     // }
 
-    public function getSkillsAttribute()
-    {
-        return $this->skills()->get();
-    }
+    // public function getSkillsAttribute()
+    // {
+    //     return $this->skills()->get();
+    // }
 
     public function getSkillIdsAttribute()
     {
