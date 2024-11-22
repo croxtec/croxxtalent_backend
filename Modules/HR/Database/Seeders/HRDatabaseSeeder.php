28dc35093827e406c7eb25a7c0d3299818bf6683
vearse <versatilekaywize94@gmail.com>
2024-11-22 19:13:49 +0100

    HR module added

---
+++ Modules/HR/Database/Seeders/HRDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\HR\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class HRDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
