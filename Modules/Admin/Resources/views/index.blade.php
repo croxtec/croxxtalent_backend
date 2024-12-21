0fc0fe19c24ba999c51f775065b442d8d1951b79
vearse <versatilekaywize94@gmail.com>
2024-12-21 15:35:56 +0100

    Modules removed

--- Modules/Admin/Resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('admin::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('admin.name') !!}</p>
-@endsection
--- Modules/HR/Resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('hr::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('hr.name') !!}</p>
-@endsection
